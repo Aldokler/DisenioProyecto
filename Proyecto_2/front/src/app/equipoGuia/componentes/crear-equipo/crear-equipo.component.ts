bf7bc9c48029d59a9ade425ec467c23739ced309
Bryan042003 <80810476+Bryan042003@users.noreply.github.com>
2023-05-13 23:37:06 -0600

    .

--- Proyecto_2/front/src/app/equipoGuia/componentes/crear-equipo/crear-equipo.component.ts
+++ Proyecto_2/front/src/app/equipoGuia/componentes/crear-equipo/crear-equipo.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { tap } from 'rxjs';
+import { ControladorService } from 'src/app/controller/controlador.service';
+import { Administrativo } from 'src/app/model/administrativo';
+import { Profesor } from 'src/app/model/profesor';
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-crear-equipo',
@@ -6,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-equipo.component.css']
 })
 export class CrearEquipoComponent {
+  constructor(
+    private controller: ControladorService
+  ) {}
 
+  public profes: Profesor[] = [];
+
+  ngOnInit(): void {
+    
+
+    this.controller.getProfesores().pipe(
+      tap(res => {
+       this.profes = res;
+      })
+    ).subscribe()
+  }
+guardarEquipo(semestre:string,annio:string){
+
+}
+  
 }
