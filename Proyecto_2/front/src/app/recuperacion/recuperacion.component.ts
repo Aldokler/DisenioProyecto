60d449f6eb165cca06cb67a1395c3a87685e1309
Bryan042003 <80810476+Bryan042003@users.noreply.github.com>
2023-05-18 07:32:49 -0600

    .

--- Proyecto_2/front/src/app/recuperacion/recuperacion.component.ts
+++ Proyecto_2/front/src/app/recuperacion/recuperacion.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminProfesores } from '../controller/AdminProfesores';
+import { ApiService } from '../controller/DAO/SERVICES/api.service';
+import { ControladorService } from 'src/app/controller/controlador.service';
+import { Usuario } from 'src/app/model/usuario';
+import { Administrativo } from 'src/app/model/administrativo';
+import { TRol } from 'src/app/model/trol';
+import { TSede } from 'src/app/model/tsede';
+import { tap } from 'rxjs';
+import { Profesor } from '../model/profesor';
+import { PasarDatosService } from '../pasar-datos.service';
 
 @Component({
   selector: 'app-recuperacion',
@@ -7,4 +18,15 @@ import { Component } from '@angular/core';
 })
 export class RecuperacionComponent {
 
+  private pasarDatos:PasarDatosService = PasarDatosService.getInstance()
+
+  constructor(private controller: ControladorService){}
+
+  ngOnInit(): void {
+
+  }
+
+  enviarCorreo(correoElectronico:string){
+    
+  }
 }
