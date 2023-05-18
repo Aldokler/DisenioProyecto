eafe4881273365f5ccd4fc2e807f2673c18c3bb4
SharonChR <sharonnathalia21@gmail.com>
2023-05-18 07:21:02 -0600

    correo

--- Proyecto_2/front/src/app/controller/DAO/email.service.ts
+++ Proyecto_2/front/src/app/controller/DAO/email.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as nodemailer from 'nodemailer';
-import { transporter } from "../../../../../back/apiRestNode/config/mailer";
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +8,5 @@ export class EmailService {
 
   constructor() { }
 
-  async sendEmail(to: string, subject: string, message: string) {
-    try {
-      // send mail with defined transport object
-  await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-});
-  } catch (error) {
-      
-  }
-
   
-  }
 }
--- Proyecto_2/front/src/app/controller/NotificadorCorreo.ts
+++ Proyecto_2/front/src/app/controller/NotificadorCorreo.ts
@@ -1,22 +1,12 @@
 import { Notificador } from "./Notificador";
-import { transporter } from "../../../../back/apiRestNode/config/mailer";
+
 
 export class NotificadorCorreo extends Notificador {
-
-    override async notificar(emisor: String, destino: String, mensaje: String): Promise<void> {
-        try {
-            // send mail with defined transport object
-        await transporter.sendMail({
-            from: '"Fred Foo 👻" <comunicador.proyecto@gmail.com>', // sender address
-            to: destino, // list of receivers
-            subject: "Hello ✔", // Subject line
-            text: mensaje // plain text body
-            //html: "<b>Hello world?</b>", // html body
-  });
-        } catch (error) {
-            
-        }
+    override notificar(emisor: String, destino: String, mensaje: String): void {
+        throw new Error("Method not implemented.");
     }
+
+    
 
 }
 
