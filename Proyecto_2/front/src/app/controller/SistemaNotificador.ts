61727d4ebe04670002bd035762d6b0324fa5588d
SharonChR <sharonnathalia21@gmail.com>
2023-06-13 21:25:56 -0600

    lis

---
+++ Proyecto_2/front/src/app/controller/SistemaNotificador.ts
@@ -0,0 +1,7 @@
+export class SistemaNotificador implements Observer{
+    public constructor(){}
+    
+    update(notificacionID: number, usuarioID: string): void {
+        throw new Error("Method not implemented.");
+    }
+}
