4ea4efd2e5a1ec0d26b4cd36fb72d10dcdd6dfd9
Bryan042003 <80810476+Bryan042003@users.noreply.github.com>
2023-06-14 02:47:15 -0600

    Sharon

--- Proyecto_2/front/src/app/home/home.component.ts
+++ Proyecto_2/front/src/app/home/home.component.ts
@@ -4,6 +4,10 @@ import { Administrativo } from 'src/app/model/administrativo';
 import { ControladorService } from 'src/app/controller/controlador.service';
 import { PasarDatosService } from '../pasar-datos.service';
 import { Profesor } from '../model/profesor';
+import { Notificacion } from '../model/notificacion';
+import swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -15,8 +19,11 @@ export class HomeComponent {
   public pasarDatos: PasarDatosService = PasarDatosService.getInstance()
   public tipoDeUsuario: string = ""
   public estadoNotificacion: boolean = false
+  public notificaciones: Notificacion[] = []
+  public notificacionesleidas: Notificacion[] = []
+  public notificacionesNoLeidas: Notificacion[] = []
 
-  constructor(private controller: ControladorService) { }
+  constructor(private controller: ControladorService, private router: Router) { }
   ngOnInit(): void {
     if (this.pasarDatos.loginUser instanceof Profesor) {
       if (this.controller.revisarCoordinador(this.pasarDatos.loginUser.getId())) {
@@ -30,6 +37,56 @@ export class HomeComponent {
     } else {
       this.tipoDeUsuario = "Estudiante"
     }
+
+    this.controller.notificaciones.getNotificaciones(this.pasarDatos.loginUser.getId()).pipe(
+      tap(res => {
+        this.notificaciones = res;
+      })
+    ).subscribe()
+
+    this.controller.notificacionesgetNotificacionesLeidas(this.pasarDatos.loginUser.getId()).pipe(
+      tap(res => {
+        this.notificacionesleidas = res;
+      })
+    ).subscribe()
+
+    this.controller.notificacionesgetNotificacionesNoLeidas(this.pasarDatos.loginUser.getId()).pipe(
+      tap(res => {
+        this.notificacionesNoLeidas = res;
+      })
+    ).subscribe()
+
+  }
+
+  borrarNotificacion(notificacion: Notificacion) {
+    if (!notificacion) {
+      this.showErrorAlert();
+      return;
+    }
+    this.controller.notificaciones.eliminarNotificacion(notificacion.getId()).subscribe(
+      () => {
+        this.showSuccessAlert();
+      }
+    )
+
+  }
+
+  showSuccessAlert() {
+    swal.fire({
+      icon: 'success',
+      title: 'Registrado con éxito',
+      timer: 2000
+    });
+    //this.router.navigate(['/ver-actividades-plan-de-trabajo']);
+  }
+
+  showErrorAlert() {
+    swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Ocurrió un error. Por favor, inténtalo nuevamente.',
+      timer: 3000
+    });
   }
 
 }
